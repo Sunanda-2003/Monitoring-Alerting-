ff72ff2edc2b7a25f30d5d2c21f5be6a752d145a
veerababu narni <147053314+CloudTechDevOps@users.noreply.github.com>
2026-06-05 11:39:10 +0530

    Update grafna-promethous.sh

--- grafna-promethous.sh
+++ grafna-promethous.sh
@@ -78,7 +78,7 @@ route:
 receivers:
   - name: pagerduty
     pagerduty_configs:
-      - routing_key: "215f782634874803d07f8b692a7124b4"
+      - routing_key: "ccc18732490d460dc00dca79aa6fd7bd"
         severity: "critical"
 EOF
 
